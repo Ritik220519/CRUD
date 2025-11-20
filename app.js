c90be382d9ea033040e07817cb638fa3cfda3013
Ritik220519 <ritik220519@gmail.com>
2025-11-20 12:45:32 +0530

    server created

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require("express");
+
+const app = express();
+
+
+app.use("/check" , (req,res)=>{
+    res.send("Server is up and running");
+})
+
+app.listen(8000 , ()=>{
+    console.log("Server is running on port 8000");
+})
